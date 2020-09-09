e37010d5fd91a2d20c4f651ddeb970677f7fcff8
Jane Chen <Jane.Chen@insightglobal.com>
2020-09-09 11:35:01 -0400

    xx

--- TollBooth/DatabaseMethods.cs
+++ TollBooth/DatabaseMethods.cs
@@ -40,10 +40,13 @@ namespace TollBooth
             {
                 // MaxItemCount value tells the document query to retrieve 100 documents at a time until all are returned.
                 // TODO 5: Retrieve a List of LicensePlateDataDocument objects from the collectionLink where the exported value is false.
+                licensePlates = _client.CreateDocumentQuery<LicensePlateDataDocument>(collectionLink,
+                    new FeedOptions() { EnableCrossPartitionQuery = true, MaxItemCount = 100 })
+                .Where(l => l.exported == false)
+                .ToList();
                 // COMPLETE: licensePlates = _client.CreateDocumentQuery ...
                 // TODO 6: Remove the line below.
-                licensePlates = new List<LicensePlateDataDocument>();
-            }
+             }
 
             exportedCount = licensePlates.Count();
             _log.LogInformation($"{exportedCount} license plates found that are ready for export");
